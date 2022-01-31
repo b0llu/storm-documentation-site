5a60a175f490927dd87b5b2b19643f09851cd3a1
Dhruv <samantdhruv@gmail.com>
2022-01-31 17:37:57 +0530

    style : added light mode color

--- docsite-js/theme.js
+++ docsite-js/theme.js
@@ -21,20 +21,20 @@ const addDarkTheme = () => {
 };
 
 const addLightTheme = () => {
-  rootDoc.style.setProperty("--default-color", "#fe0808");
-  rootDoc.style.setProperty("--white-black", "#ffffff");
+  rootDoc.style.setProperty("--default-color", "#ffffff");
+  rootDoc.style.setProperty("--white-black", "#171616");
   rootDoc.style.setProperty("--red", "#ff002b");
-  rootDoc.style.setProperty("--primary-color", "#55E5F5");
+  rootDoc.style.setProperty("--primary-color", "#36c4d3");
   rootDoc.style.setProperty("--primary-color-dark", "#66e8f6");
   rootDoc.style.setProperty("--secondary-color", "#c3c5c7");
   rootDoc.style.setProperty("--alert-color", "#e65147");
   rootDoc.style.setProperty("--success-color", "#4caf50");
   rootDoc.style.setProperty("--warning-color", "#ff9800");
-  rootDoc.style.setProperty("--nav-bg-color", "#272727");
+  rootDoc.style.setProperty("--nav-bg-color", "#41535c");
   rootDoc.style.setProperty("--header-color", "#238ac5");
-  rootDoc.style.setProperty("--content-color", "#b6bab3");
+  rootDoc.style.setProperty("--content-color", "#000000");
   rootDoc.style.setProperty("--card-container-bg", "#aeabab");
-  rootDoc.style.setProperty("--text-color", "#1c1b1b");
+  rootDoc.style.setProperty("--text-color", "#000000");
 };
 
 const darkThemeToggleBtn = () => {
