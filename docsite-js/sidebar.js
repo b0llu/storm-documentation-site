2a852ea16ffa0bf2d6158e4d0e4f82f346360200
Dhruv <samantdhruv@gmail.com>
2022-02-06 20:03:19 +0530

    fix : body movement fix on mobile device

--- docsite-js/sidebar.js
+++ docsite-js/sidebar.js
@@ -1,13 +1,16 @@
 const sidebarToggle = document.querySelector("#sidebar-toggle");
 const sidebarMenu = document.querySelector("#sidebar-menu");
 const closeSidebar = document.querySelector('#close-sidebar');
+const body = document.querySelector('body')
 
 sidebarToggle.addEventListener("click", () => {
     sidebarMenu.classList.add("position-open");
     sidebarMenu.classList.remove("position-hidden");
+    body.classList.add("position-fixed")
 });
 
 closeSidebar.addEventListener('click', () => {
     sidebarMenu.classList.add("position-hidden");
     sidebarMenu.classList.remove("position-open");
+    body.classList.remove("position-fixed")
 })
